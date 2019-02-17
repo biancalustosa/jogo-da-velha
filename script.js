d161684427459870fa514ef78da69f8242adbb0e
Bianca Lustosa <biafelix08@gmail.com>
2019-02-17 15:54:10 -0300

    Jogo-da-Velha

---
+++ script.js
@@ -0,0 +1,72 @@
+let simbolo = prompt('Você é o jogador um. Digite X ou O para começar.');
+// let jogadorDois = prompt('Você é o jogador dois. Escolha X ou O para começar.');
+
+const quadUm = document.querySelector ('.quad-um');
+// const quadDois = document.querySelector ('.quad-dois');
+// const quadTres = document.querySelector ('.quad-tres');
+// const quadQuatro = document.querySelector ('.quad-quatro');
+// const quadCinco= document.querySelector ('.quad-cinco');
+// const quadSeis = document.querySelector ('.quad-seis');
+// const quadSete = document.querySelector ('.quad-sete');
+// const quadOito = document.querySelector ('.quad-oito');
+// const quadNove = document.querySelector ('.quad-nove');
+
+
+let jogadorUm = "X" ;
+let jogadorDois = "O";
+
+}
+// let quadDois;
+// let quadTres;
+// let quadQuatro;
+// let quadCinco;
+// let quadSeis;
+// let quadSete;
+// let quadOito;
+// let quadNove;
+
+
+// const um = X;
+// const dois = O;
+
+// let jogadorUm;
+// let jogadorDois;
+
+// while( simbolo == jogadorUm && simbolo == jogadorDois){
+//     // console.log(jogadorUm, jogadorDois);
+
+    if(simbolo == jogadorUm){
+        alert('Você é o X.');
+    }
+
+    else{
+        alert('Você é a O.');
+    }
+
+//     if(quadUm, quadDois, quadTres == jogadorUm || quadUm, quadDois, quadTres == jogadorDois || quadQuatro, quadCinco, quadSeis == jogadorUm || quadQuatro, quadCinco, quadSeis == jogadorDois|| quadSete, quadOito, quadNove == jogadorUm || quadSete, quadOito, quadNove == jogadorDois*/){
+
+//         alert('Parabéns. Você ganhou o jogo.');
+
+//             if(quadUm, quadQuatro, quadNove == escolha || quadUm, quadQuatro, quadNove == escolhaDois || quadTres, quadCinco, quadSete == escolha || quadTres, quadCinco, quadSete == escolhaDois){
+
+//              alert('Parabéns. Você ganhou o jogo.');
+
+//              if(quadUm, quadQuatro, quadSete == escolha|| quadUm, quadQuatro, quadSete == escolhaDois || quadDois, quadCinco, quadOito == escolha || quadDois, quadCinco, quadOito == escolhaDois || quadTres, quadSeis, quadNove == escolha || quadTres, quadSeis, quadNove == escolhaDois){
+//                     alert('Parabéns. Você ganhou o jogo.');
+//                 }
+//                 else{
+//                     alert('Vocêr perdeu.');
+//                 }
+
+//             }
+
+//     }
+
+    else{
+        alert('Deu velha. O jogo acabou.');
+    }
+    
+// }
+
+quadUm.onclick = jogadorUm;
+// quadUm.onclick = escolhaDois;
